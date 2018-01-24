b4f76688d29bafc282c21cd9ca6325d5b1b717a9
adrianno3259 <adriannosampaio@hotmail.com>
2018-01-23 23:08:17 -0200

    Add Ray Class

---
+++ include/Ray.h
@@ -0,0 +1,32 @@
+#ifndef RAY_H
+#define RAY_H
+
+#include "Vec3d.h"
+/**
+*   Mathematical representation of a light ray with
+*   an origin point and a direction vector
+*/
+class Ray
+{
+    public:
+        /// Origin point of the ray
+        Vec3d origin;
+        /// Direction vector of the ray
+        Vec3d direction;
+
+        /**
+        *   Default constructor
+        *   Sets a ray starting on the origin point o
+        *   and following the direction of the vector d
+        */
+        Ray(const Vec3d& o, const Vec3d& d);
+
+        /**
+        *   Calculates the point corresponding to
+        *   the parameter t in the ray
+        */
+        Vec3d rayPoint(double t) const;
+
+};
+
+#endif // RAY_H
---
+++ src/Ray.cpp
@@ -0,0 +1,14 @@
+#include "Ray.h"
+
+Ray::Ray(const Vec3d& o, const Vec3d& d) :
+    origin(o), direction(d) { }
+
+Vec3d Ray::rayPoint(double t) const
+{
+    return origin + t * direction;
+}
+
+Ray::~Ray()
+{
+    //dtor
+}
