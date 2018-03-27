3051db73a2a6a5f32cce6ba669373e0b9eed8ddb
adrianno3259 <adriannosampaio@hotmail.com>
2018-03-27 15:34:52 -0300

    Add new 2k model and improve script

--- bunny_tests.sh
+++ bunny_tests.sh
@@ -1,32 +1,18 @@
+#!/bin/bash
+obj=3d_models
+ren=renders
+
 for k in `seq 1 3`;
 do
-
-	#Pumpkin
-	./raytracer 3d_models/pumpkin.obj renders/fr_pumpkin_200_200.ppm 200 200 0 3 1 1.1
-	./raytracer 3d_models/pumpkin.obj renders/fr_pumpkin_800_600.ppm 800 600 0 2.5 2 0.5
-	./raytracer 3d_models/pumpkin.obj renders/fr_pumpkin_1920_1080.ppm 1920 1080 0 2.5 2 0.14
-
-
-	#Teddy bear
-	./raytracer 3d_models/teddy.obj renders/fr_teddy_200_200.ppm 200 200 0 3 3 1.1
-	./raytracer 3d_models/teddy.obj renders/fr_teddy_800_600.ppm 800 600 0 3 3 0.5
-	./raytracer 3d_models/teddy.obj renders/fr_teddy_1920_1080.ppm 1920 1080 0 3 3 0.14
-
-
-	#bunnies
-
-	obj=3d_models
-	ren=renders
-
-	for i in 5k 10k 50k;
+	for i in 2k 10k 50k;
 	do
 		echo $i;
 		prefix=fr_bunny_
 		sufix=_200_200.ppm
-		./raytracer $obj/bunny/bunny_$i.obj $ren/$prefix$i$sufix 200 200 0 2.5 2.5 0.9
+		#./raytracer $obj/bunny/bunny_$i.obj $ren/$prefix$i$sufix 200 200 0 2.5 2.5 0.9
 		sufix=_800_600.ppm
 		./raytracer $obj/bunny/bunny_$i.obj $ren/$prefix$i$sufix 800 600 0 2.5 2.5 0.4
 		sufix=_1920_1080.ppm
-		./raytracer $obj/bunny/bunny_$i.obj $ren/$prefix$i$sufix 1920 1080 0 2.5 2.5 0.15
+		#./raytracer $obj/bunny/bunny_$i.obj $ren/$prefix$i$sufix 1920 1080 0 2.5 2.5 0.15
 	done
 done
--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <cstdlib>
 
 #include <utility>
-
+#include <ctime>
 #include <cmath>
 
 #include "include/Vec3d.h"
@@ -27,6 +27,8 @@
 #define PV(A) cout<<#A<<" = "<<A<endl
 #define FOR(I,N) for(int I = 0; I < N; I++)
 
+#define INV_PI 0.318309886
+
 /// initializing the triangle instance counter
 //int Triangle::TRIANGLE_NEXT_ID = 0;
 int Mesh::MESH_NEXT_ID = 0;
@@ -35,47 +37,95 @@ using namespace std;
 
 #define forObj(mesh) cout<<"mesh id = "<<mesh->getId()<<"\n";for(int i = 0; i < 5; i++){mesh->triangles[i]->printData();}
 
-int main(){
+vector<shared_ptr<Mesh> > meshes;
+/*
+vector<shared_ptr<Light> >    lights;
+vector<shared_ptr<Material> > materials;
+*/
 
-    Vec3d eye(0, -30, 0), lkp(0, 0, 0), up(0,0,1);
-    double dist = 50, psize=1;
-    int vres = 500, hres = 500;
+int main(int argc, char** argv){
+
+    string obj, output;
+    int camx = 4, camy = 0, camz = 3;
+    int hres = atoi(argv[3]), vres = atoi(argv[4]);
+    double psize = 1.0;
+    if(argc > 1 )
+    {
+        obj = argv[1];
+        output = argv[2];
+        camx = atof(argv[5]);
+        camy = atof(argv[6]);
+        camz = atof(argv[7]);
+        psize = atof(argv[8]);
+    }
+    else
+    {
+        obj = "3d_models/teddy.obj";
+        output = "image.ppm";
+    }
+
+
+    Vec3d eye(camx, camy, camz), lkp(0, 0, 0), up(0,0,1);
+    double dist = 200;
+
     Camera cam = Camera(eye, lkp, up, dist, psize, vres, hres);
 
+    shared_ptr<Mesh> m = make_shared<Mesh>(obj);
 
+    shared_ptr<Image> im = make_shared<Image>(hres, vres);
 
-    shared_ptr<Mesh> m = make_shared<Mesh>("3d_models/teddy.obj");
+    shared_ptr<Light> light = make_shared<Light>(2.0, Color(1.0), Vec3d(50.0, 50.0, 50.0));
 
-    shared_ptr<Image> im = make_shared<Image>(vres, hres);
+    Color matCol = RED;
+    double matDif = 0.7;
 
-    shared_ptr<Light> light = make_shared<Light>(1.0, Color(1.0), Vec3d(10.0, 10.0, 10.0));
+    struct timespec start, stop;
 
-    for(int r = 0; r < vres; r++)
-    for(int c = 0; c < hres; c++)
+    clock_gettime(CLOCK_MONOTONIC, &start);//time = clock();
+
+    int r, c;
+    for(r = 0; r < vres; r++)
+    for(c = 0; c < hres; c++)
     {
         Color L;
         Ray ray = cam.getRay(r, c);
+
         double tMin = 10000000.0;
-        int idMin = -1;
-        //cout<<m.triangles.size();
+        int idMin = -1, iMin = -1;
         for(int i = 0; i < m->triangles.size(); i++)
         {
             double t;
             bool hit = m->triangles[i]->hit(ray, t);
-            //if(hit) cout<<"t = "<<t<<endl;
             if(hit && t < tMin)
             {
-                //cout<<idMin<<" "<<t<<"\n";
                 idMin = m->triangles[i]->getId();
                 tMin = t;
-                L = Color(1.0);
+                iMin = i;
             }
         }
-        im->setPixel(r, c, L);
+
+        if(idMin != -1)
+        {
+            Vec3d wo = -ray.direction;
+            double dot = m->triangles[iMin]->normal * wo;
+            Color lightInfluence = light->mColor * light->mIntensity;
+            L = (matCol * matDif * INV_PI) * lightInfluence * dot;
+        }
+
+        im->setPixel(c, r, L);
 
     }
 
-    im->save("image.ppm");
+    clock_gettime(CLOCK_MONOTONIC, &stop);
+    float tm = (stop.tv_sec - start.tv_sec) + (stop.tv_nsec - start.tv_nsec) / 1e9;
+    printf("Rendering %s with resolution %dx%d took %f seconds\n",
+            obj.c_str(),
+            hres,
+            vres,
+            tm);
+
+    cout<<"Saving "<<output<<"\n";
+    im->save(output.c_str());
 
     return 0;
 }
