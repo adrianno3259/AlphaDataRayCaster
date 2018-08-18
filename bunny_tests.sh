5a441b376e0fd9fc3eb9bfc6de4c6e4e78aa0c51
adrianno3259 <adriannosampaio@hotmail.com>
2018-08-18 12:52:14 -0300

    Add FPGA main code example

--- bunny_tests.sh
+++ bunny_tests.sh
@@ -9,10 +9,10 @@ do
 		echo $i;
 		prefix=fr_bunny_
 		sufix=_200_200.ppm
-		#./raytracer $obj/bunny/bunny_$i.obj $ren/$prefix$i$sufix 200 200 0 2.5 2.5 0.9
+		./raytracer $obj/bunny/bunny_$i.obj $ren/$prefix$i$sufix 200 200 0 2.5 2.5 0.9
 		sufix=_800_600.ppm
 		./raytracer $obj/bunny/bunny_$i.obj $ren/$prefix$i$sufix 800 600 0 2.5 2.5 0.4
 		sufix=_1920_1080.ppm
-		#./raytracer $obj/bunny/bunny_$i.obj $ren/$prefix$i$sufix 1920 1080 0 2.5 2.5 0.15
+		./raytracer $obj/bunny/bunny_$i.obj $ren/$prefix$i$sufix 1920 1080 0 2.5 2.5 0.15
 	done
 done
---
+++ compileFPGA.sh
@@ -0,0 +1,1 @@
+g++ -I/home/fpgadev/Documents/Adrianno/admpcieku3_sdk-2.0.0/host/api-v1_4_18b9/include testeAD.cpp -m64 -o ray -ladmxrc3 -lc -ldl -lpthread
